e014b3a771d7e9cda578948908ec299cf0d2c531
Christian Fryksten <christianfryksten1@gmail.com>
2025-03-27 14:33:17 +0100

    HTML and style

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,18 @@
+import { useState } from 'react';
 import './App.css';
 import Header from './header.jsx';
 import BackgroundMusic from './BackgroundMusic.jsx';
 
 function App() {
+  const [gameStarted, setGameStarted] = useState(false);
+
   return (
     <div className="app">
       <BackgroundMusic />
       <Header />
       <main>
-        <p>Test paragraph!</p>
+        <p className="start-text">Are you smart enough to Wordle?</p>
+        <button className="start-game-button">Start game!</button>
       </main>
     </div>
   );
