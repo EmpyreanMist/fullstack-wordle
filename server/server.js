e6c517142aea1d7ef2deeba061ccb24afeb1413d
Christian Fryksten <christianfryksten1@gmail.com>
2025-03-28 10:12:39 +0100

    Api path to get random word

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import gameRoute from './routes/gameRoute.js';
 import highScoreRoute from './routes/highScoreRoute.js';
 import infoRoute from './routes/infoRoute.js';
+import wordRoute from './routes/wordRoute.js';
 
 const port = 5080;
 const app = express();
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use('/api/game', gameRoute);
 app.use('/api/highscore', highScoreRoute);
 app.use('/api/info', infoRoute);
+app.use('/api/word', wordRoute);
 
 app.get('/', (req, res) => {
   res.send('Tjenare');
