33370dab80a7a6a77ed123132f4ce48dad3d5a0e
Christian Fryksten <christianfryksten1@gmail.com>
2025-03-28 09:56:26 +0100

    Refactor getRandomWord

--- server/wordService.js
+++ server/wordService.js
@@ -1,19 +1,17 @@
 import fs from 'fs';
 
+const data = fs.readFileSync('./data/words.txt', 'utf-8');
+const wordsArray = data.split('\n').map((word) => word.trim());
+
 function getRandomWord(length) {
-  const data = fs.readFileSync('./data/words.txt', 'utf-8');
-  const wordsArray = data.split('\n').map((word) => word.trim());
-
-  const wordsWithSameLength = [];
-
-  wordsArray.forEach((word) => {
-    if (word.length === length) {
-      wordsWithSameLength.push(word);
-    }
-  });
+  const wordsWithSameLength = wordsArray.filter(
+    (word) => word.length === length
+  );
 
   let randomWord =
     wordsWithSameLength[Math.floor(Math.random() * wordsWithSameLength.length)];
 
   return randomWord;
 }
+
+export default getRandomWord;
