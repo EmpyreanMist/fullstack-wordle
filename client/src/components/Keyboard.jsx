3bf5f9175451fe1d1174ce19bdca43f988421893
EmpyreanMist <christianfryksten1@gmail.com>
2025-03-29 12:49:16 +0100

    Typos in Keyboard

--- client/src/components/Keyboard.jsx
+++ client/src/components/Keyboard.jsx
@@ -1,7 +1,7 @@
 function Keyboard() {
   const rowOne = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
   const rowTwo = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
-  const rowThree = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
+  const rowThree = ["Z", "X", "C", "V", "B", "N", "M"];
   return (
     <div className="keyboard">
       <div className="rowOne">
@@ -22,7 +22,7 @@ function Keyboard() {
           );
         })}
       </div>
-      <div className="rowTwo">
+      <div className="rowThree">
         {rowThree.map((letter) => {
           return (
             <button className="key" key={letter}>
