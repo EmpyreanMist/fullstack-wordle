6b9e56537374c6275999d6a6dd21098598a0138c
EmpyreanMist <christianfryksten1@gmail.com>
2025-03-29 13:13:55 +0100

    Enter and backspace included in keyboard

--- client/src/components/Keyboard.jsx
+++ client/src/components/Keyboard.jsx
@@ -1,29 +1,44 @@
 function Keyboard() {
   const rowOne = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
   const rowTwo = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
-  const rowThree = ["Z", "X", "C", "V", "B", "N", "M"];
+  const rowThree = ["ENTER", "Z", "X", "C", "V", "B", "N", "M", "BACKSPACE"];
+
   return (
     <div className="keyboard">
       <div className="rowOne">
-        {rowOne.map((letter) => {
-          return (
-            <button className="key" key={letter}>
-              {letter}
-            </button>
-          );
-        })}
+        {rowOne.map((letter) => (
+          <button className="key" key={letter}>
+            {letter}
+          </button>
+        ))}
       </div>
+
       <div className="rowTwo">
-        {rowTwo.map((letter) => {
-          return (
-            <button className="key" key={letter}>
-              {letter}
-            </button>
-          );
-        })}
+        {rowTwo.map((letter) => (
+          <button className="key" key={letter}>
+            {letter}
+          </button>
+        ))}
       </div>
+
       <div className="rowThree">
         {rowThree.map((letter) => {
+          if (letter === "ENTER") {
+            return (
+              <button className="key enter-key" key={letter}>
+                ENTER
+              </button>
+            );
+          }
+
+          if (letter === "BACKSPACE") {
+            return (
+              <button className="key backspace-key" key={letter}>
+                ⌫
+              </button>
+            );
+          }
+
           return (
             <button className="key" key={letter}>
               {letter}
