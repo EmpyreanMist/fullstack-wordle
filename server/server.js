4f729285d72f08107ae6e8337fb9cf35090704ef
Christian Fryksten <christianfryksten1@gmail.com>
2025-04-04 14:13:50 +0200

    Start of SSR scoreboard

--- server/server.js
+++ server/server.js
@@ -1,3 +1,6 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 import express from 'express';
 import cors from 'cors';
 
@@ -9,6 +12,12 @@ import wordRoute from './routes/wordRoute.js';
 const port = 5080;
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.use(cors());
 app.use(express.json());
 
@@ -19,6 +28,16 @@ app.use('/api/word', wordRoute);
 
 app.get('/', (req, res) => {
   res.send('Tjenare');
+});
+
+app.get('/scoreboard', (req, res) => {
+  const dummyScores = [
+    { name: 'Alice', guesses: 3, wordLength: 5, timeInSeconds: 12 },
+    { name: 'Bob', guesses: 4, wordLength: 6, timeInSeconds: 18 },
+    { name: 'Eve', guesses: 2, wordLength: 5, timeInSeconds: 10 },
+  ];
+
+  res.render('scoreboard', { scores: dummyScores });
 });
 
 app.listen(port, () => {
